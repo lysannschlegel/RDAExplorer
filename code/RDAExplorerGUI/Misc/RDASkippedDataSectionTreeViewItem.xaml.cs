a529b3485562f79c128f7e0e35a14e515bb15a16
Lysann Schlegel <lysannschlegel@users.noreply.github.com>
2015-11-12 23:34:37 +0100

    add an option to delete an encrypted block

---
+++ code/RDAExplorerGUI/Misc/RDASkippedDataSectionTreeViewItem.xaml.cs
@@ -0,0 +1,22 @@
+﻿using RDAExplorer;
+using System.Windows;
+
+namespace RDAExplorerGUI.Misc
+{
+    public partial class RDASkippedDataSectionTreeViewItem
+    {
+        public RDASkippedDataSection Section;
+
+        public RDASkippedDataSectionTreeViewItem()
+        {
+            InitializeComponent();
+            SelectOnRightClick = true;
+        }
+
+        private void context_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.CurrentMainWindow.CurrentReader.SkippedDataSections.Remove(Section);
+            MainWindow.CurrentMainWindow.RebuildTreeView();
+        }
+    }
+}
