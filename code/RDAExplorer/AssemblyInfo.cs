bb1bf1ea2e0c177855c9114e658b9e2a8e648010
Lysann Schlegel <lysannschlegel@users.noreply.github.com>
2015-11-15 16:25:38 +0100

    bump version to 1.2.0.0

--- code/RDAExplorer/AssemblyInfo.cs
+++ code/RDAExplorer/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Security.Permissions;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("528de4c4-9ebe-48d5-8465-061681163cf2")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
