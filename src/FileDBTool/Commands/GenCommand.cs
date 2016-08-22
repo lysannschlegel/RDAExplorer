17d4473da64a4bd678303d82f26e2623106bc058
Lysann Schlegel <lysann.schlegel@live.de>
2016-08-23 00:20:03 +0200

    FileDBTool gen can process a directory of RDA files

--- src/FileDBTool/Commands/GenCommand.cs
+++ src/FileDBTool/Commands/GenCommand.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace RDAExplorer.FileDBTool.Commands
 {
     class GenFileDBCommand : ManyConsole.ConsoleCommand
@@ -9,28 +12,43 @@ namespace RDAExplorer.FileDBTool.Commands
             IsCommand("gen", "Generate file.db from RDA files");
             HasLongDescription("Generate file.db to standard output from RDA files given as parameters.");
 
-            AllowsAnyAdditionalArguments("[data0.rda [data1.rda [...]]]");
+            AllowsAnyAdditionalArguments("(RDA files)|(directory containing RDA files)");
 
             HasRequiredOption("output|o=", "The output file name", (value) => { this.outputFileName = value; });
         }
 
         public override int Run(string[] remainingArguments)
         {
-            var fileSystem = new AnnoRDA.FileSystem();
-            var fileLoader = new AnnoRDA.Loader.ContainerFileLoader();
+            IList<string> containerPaths;
+            AnnoRDA.FileSystem fileSystem;
 
-            foreach (var rdaFileName in remainingArguments) {
-                var containerFileSystem = fileLoader.Load(rdaFileName);
-                fileSystem.Merge(containerFileSystem, System.Threading.CancellationToken.None);
+            if (remainingArguments.Length == 1 && this.PathIsDirectory(remainingArguments[0])) {
+                var directoryLoader = new AnnoRDA.Loader.ContainerDirectoryLoader();
+                IEnumerable<string> enumerableContainerPaths;
+                fileSystem = directoryLoader.Load(remainingArguments[0], System.Threading.CancellationToken.None, out enumerableContainerPaths);
+                containerPaths = enumerableContainerPaths.ToArray();
+            } else {
+                containerPaths = remainingArguments;
+                fileSystem = new AnnoRDA.FileSystem();
+                var fileLoader = new AnnoRDA.Loader.ContainerFileLoader();
+                foreach (var rdaFileName in containerPaths) {
+                    var containerFileSystem = fileLoader.Load(rdaFileName);
+                    fileSystem.Merge(containerFileSystem, System.Threading.CancellationToken.None);
+                }
             }
 
             using (var outputStream = new System.IO.FileStream(this.outputFileName, System.IO.FileMode.Create, System.IO.FileAccess.Write)) {
                 using (var writer = new AnnoRDA.FileDB.Writer.FileDBWriter(outputStream, false)) {
-                    writer.WriteFileDB(fileSystem, remainingArguments);
+                    writer.WriteFileDB(fileSystem, containerPaths);
                 }
             }
 
             return 0;
+        }
+
+        private bool PathIsDirectory(string path)
+        {
+            return (System.IO.File.GetAttributes(path) & System.IO.FileAttributes.Directory) == System.IO.FileAttributes.Directory;
         }
     }
 }
