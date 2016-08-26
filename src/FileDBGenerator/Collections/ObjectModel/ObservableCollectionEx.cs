70985360ec60fb31113012dc42bea5f0d39f0e0b
Lysann Schlegel <lysann.schlegel@live.de>
2016-08-26 21:34:34 +0200

    add FileDBGenerator GUI app to generate file.db

---
+++ src/FileDBGenerator/Collections/ObjectModel/ObservableCollectionEx.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace FileDBGenerator.Collections.ObjectModel
+{
+    class ObservableCollectionEx<T> : ObservableCollection<T> where T : INotifyPropertyChanged
+    {
+        public ObservableCollectionEx()
+        {
+            this.CollectionChanged += ObservableCollectionEx_CollectionChanged;
+        }
+
+        private void ObservableCollectionEx_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null) {
+                foreach (object item in e.OldItems) {
+                    ((INotifyPropertyChanged)item).PropertyChanged -= ItemPropertyChanged;
+                }
+            }
+            if (e.NewItems != null) {
+                foreach (object item in e.NewItems) {
+                    ((INotifyPropertyChanged)item).PropertyChanged += ItemPropertyChanged;
+                }
+            }
+        }
+
+        private void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(
+                action: NotifyCollectionChangedAction.Replace,
+                newItem: sender,
+                oldItem: sender,
+                index: this.IndexOf((T)sender)
+            ));
+        }
+    }
+}
---
+++ src/FileDBGenerator/ViewModels/RDAFileList.cs
@@ -0,0 +1,43 @@
+﻿using FileDBGenerator.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace FileDBGenerator.ViewModels
+{
+    class RDAFileListItem : INotifyPropertyChanged
+    {
+        private bool isEnabled;
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set {
+                this.isEnabled = value;
+                this.NotifyPropertyChanged("IsEnabled");
+            }
+        }
+        public string LoadPath { get; }
+        public string Name { get; }
+
+        internal RDAFileListItem(bool isEnabled, string loadPath, string name)
+        {
+            this.IsEnabled = isEnabled;
+            this.LoadPath = loadPath;
+            this.Name = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class RDAFileList
+    {
+        public ObservableCollectionEx<RDAFileListItem> Items { get; }
+
+        public RDAFileList()
+        {
+            this.Items = new ObservableCollectionEx<RDAFileListItem>();
+        }
+    }
+}
