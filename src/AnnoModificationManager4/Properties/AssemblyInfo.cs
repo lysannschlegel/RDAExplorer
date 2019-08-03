aba2886d48aff9b1614d1c2d3173fd14062300d5
Lysann Schlegel <lysannschlegel@users.noreply.github.com>
2019-08-03 23:28:05 +0200

    bump/set version numbers

--- src/AnnoModificationManager4/Properties/AssemblyInfo.cs
+++ src/AnnoModificationManager4/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AnnoModificationManager4")]
-[assembly: AssemblyCopyright("Copyright © 2010-2015 Ruman Gerst, D. Schneider, Lysann Schlegel")]
+[assembly: AssemblyCopyright("Copyright © 2010-2019 Ruman Gerst, D. Schneider, Lysann Schlegel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
